a6dab21ce3888cce4d3c3d5da422cbdca86fd309
DVanam <hisokadara@gmail.com>
2022-09-27 13:40:15 +0300

    Creation day - 27.09.22

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException, URISyntaxException, AWTException {
+//        Проверка поддерживаеться ил нашей системой этот Tray.
+        if (SystemTray.isSupported()) {
+//            Получить наш Системный Трей.
+            SystemTray tray = SystemTray.getSystemTray();
+//            Выбор иконки.
+            Image image = new ImageIcon("src/logo.png", "desc").getImage();
+//            PopupMenu -Выбор Миню правым кликом. Различные Меню.
+            PopupMenu popupMenu = new PopupMenu();
+
+
+            MenuItem menuItem = new MenuItem("One");
+            menuItem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFrame jframe = getjFrame();
+                }
+            });
+
+            popupMenu.add(menuItem);
+            popupMenu.add(new MenuItem("Two"));
+            popupMenu.add(new MenuItem("Three"));
+
+            
+
+//            tray.add-Выведения.
+//            image-иконка.
+//            "Tray description"-При наведение выводет Информацию.
+//            popupMenu-Меню кнопак.
+            tray.add(new TrayIcon(image, "Tray description", popupMenu));
+        }
+    }
+
+    //    JFrame -Создание Окна + размещение его.
+    public static JFrame getjFrame() {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+//        jFrame.setSize(500,300); // Размер окна.
+//        jFrame.setLocation(500, 400); // Область на Экране.
+
+//        Toolkit -Набор для работы с нашими приложениями.
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+//        Dimension -Размен нашего окна. Есть ширина и высота нашего Экрана.
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+//        Задаем размер по 4 сторонам Экрана.
+        jFrame.setBounds(dimension.width /2 - 250 ,dimension.height /2 - 150, 500, 300);
+
+
+        jFrame.setTitle("this my app"); // Названия.
+//        jFrame.setIconImage(" ");// картинка
+        return jFrame;
+    }
+
+
+}
